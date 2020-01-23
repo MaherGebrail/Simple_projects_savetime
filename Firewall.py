4e75461586fda073bba1deb27c16fa39d58f5840
MaherGebrail <mahersafwatmaher@gmail.com>
2020-01-23 21:04:48 +0200

    Edited reporting

--- Firewall.py
+++ Firewall.py
@@ -2,44 +2,53 @@
 import subprocess
 import os
 import time
+import json
 
-ips = []
 
-subprocess.Popen(f'echo "[[ Report ]]\n\n"> report_checking_actions', shell=True, stdin=subprocess.PIPE,
-                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def start_():
+    if not os.path.isfile("Ips_log.json"):
+        with open("Ips_log.json", "w") as f:
+            ips = {"checked ips": [],
+                   "checking ips": [],
+                   "blacklisted": []}
+            json.dump(ips, f)
+    else:
+        with open("Ips_log.json", "r") as f:
+            ips = json.load(f)
+            if not ips:
+                ips = {"checked ips": [],
+                       "checking ips": [],
+                       "blacklisted": []}
+    return ips
 
 
 def app_():
-    subprocess.Popen('sudo netstat -atup  > netstate_log.txt', shell=True, stdin=subprocess.PIPE,
-                     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    try:
-        with open('netstate_log.txt', 'r') as f:
-            f = f.readlines()
-            lines = []
-            for line in f[2:]:
-                line_list = line.split(" ")
-                line_list = [i for i in line_list if i != '']
-                lines.append(line_list)
-    except FileNotFoundError:
-        return
-    print("************\nChecked ips : ", ips)
+    ips = start_()
+    f = subprocess.check_output('sudo netstat -atup ', shell=True,
+                                stdin=subprocess.PIPE, stderr=subprocess.PIPE).decode()
+    # print(f)
+    lines = []
+    for line in f.split("\n")[2:]:
+        line_list = line.split(" ")
+        line_list = [i for i in line_list if i != '']
+        lines.append(line_list)
 
-    counter = 0
     for i in lines:
         if len(i) == 8 and '[::]' not in i[4]:
             x = i[4].split(":")
-            if x[0] not in ips and x[0] != "0.0.0.0" and x[0] != 'localhost':
-                counter += 1
-                ips.append(x[0])
+            if x[0] not in ips['checked ips'] and x[0] not in ips["checking ips"] and x[0] != "0.0.0.0" \
+                    and x[0] != 'localhost':
+                ips["checking ips"].append(x[0])
 
-    if counter > 0:
-        print("\n**********\nChecking Ips : ", ips[-counter:])
-        pass
+    if ips["checking ips"]:
+        print("\n**********\nChecking Ips : ", ips["checking ips"])
+        with open('Ips_log.json', 'w') as f:
+            json.dump(ips, f)
     else:
         print('None new ip interact to check ...')
         return
-    for i in range(1, counter + 1):
-        ip = ips[-i]
+
+    for ip in ips["checking ips"]:
         print(f"Checking {ip}")
         try:
             check_output = subprocess.check_output(f"amispammer -d {ip}", shell=True, stdin=subprocess.PIPE,
@@ -47,28 +56,21 @@ def app_():
         except subprocess.CalledProcessError:
             check_output = subprocess.check_output(f"amispammer -i {ip}", shell=True, stdin=subprocess.PIPE,
                                                    stderr=subprocess.PIPE)
-        for line_o in check_output.decode().split('\n'):
-            subprocess.Popen(f'echo "{line_o}" >> report_checking_actions', shell=True, stdin=subprocess.PIPE,
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            lines = check_output.decode().split('\n')
+
+        lines = check_output.decode().split('\n')
+        ips["checked ips"].append(ip)
 
         if 'REASON' in check_output.decode():
             spam_ip = lines[2].split(":")[1]
-
             subprocess.Popen(f"sudo iptables -A INPUT -s {spam_ip} -j DROP", shell=True, stdin=subprocess.PIPE,
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            subprocess.Popen(
-                f'echo "---------------->> IP (blacklisted) : {spam_ip} DROPPED IN iptables " >> report_checking_actions',
-                shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
+            subprocess.Popen(f"sudo iptables -A INPUT -d {spam_ip} -j DROP", shell=True, stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             print("----------> Blocked IP : ", spam_ip)
-        else:
-            subprocess.Popen(
-                f'echo "{lines[2].split(":")[1][1:]} .. is Safe" >> report_checking_actions',
-                shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        subprocess.Popen('echo " \n              **********************************'
-                         '************************ \n" >> report_checking_actions', shell=True, stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            ips["blacklisted"].append(spam_ip)
+    ips["checking ips"] = []
+    with open("Ips_log.json", "w") as f:
+        json.dump(ips, f)
 
 
 while True:
