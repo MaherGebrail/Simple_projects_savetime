4c30163848d503bbb57b4b865c29dc34c3ed3b97
MaherGebrail <mahersafwatmaher@gmail.com>
2020-01-24 14:57:07 +0200

    edited

--- Firewall.py
+++ Firewall.py
@@ -6,27 +6,29 @@ import json
 
 
 def start_():
-    if not os.path.isfile("Ips_log.json"):
-        with open("Ips_log.json", "w") as f:
-            ips = {"checked ips": [],
-                   "checking ips": [],
-                   "blacklisted": []}
-            json.dump(ips, f)
-    else:
+    """it's create the log file, or read it if existed the """
+    ips_ = {"checked ips": [],
+            "checking ips": [],
+            "blacklisted": []}
+    if os.path.isfile("Ips_log.json"):
         with open("Ips_log.json", "r") as f:
             ips = json.load(f)
             if not ips:
-                ips = {"checked ips": [],
-                       "checking ips": [],
-                       "blacklisted": []}
+                ips = ips_
+    else:
+        with open("Ips_log.json", "w") as f:
+            ips = ips_
+            json.dump(ips, f)
+
     return ips
 
 
 def app_():
+    """It blocks the server's ip , not the host name, so may block some domains if they are attached to poisoned ip """
     ips = start_()
-    f = subprocess.check_output('sudo netstat -atup ', shell=True,
+    f = subprocess.check_output('sudo netstat -atupn ', shell=True,
                                 stdin=subprocess.PIPE, stderr=subprocess.PIPE).decode()
-    # print(f)
+
     lines = []
     for line in f.split("\n")[2:]:
         line_list = line.split(" ")
@@ -36,8 +38,7 @@ def app_():
     for i in lines:
         if len(i) == 8 and '[::]' not in i[4]:
             x = i[4].split(":")
-            if x[0] not in ips['checked ips'] and x[0] not in ips["checking ips"] and x[0] != "0.0.0.0" \
-                    and x[0] != 'localhost':
+            if x[0] not in ips['checked ips'] + ips["checking ips"] + ips["blacklisted"] + ["0.0.0.0", 'localhost']:
                 ips["checking ips"].append(x[0])
 
     if ips["checking ips"]:
@@ -50,22 +51,19 @@ def app_():
 
     for ip in ips["checking ips"]:
         print(f"Checking {ip}")
-        try:
-            check_output = subprocess.check_output(f"amispammer -d {ip}", shell=True, stdin=subprocess.PIPE,
-                                                   stderr=subprocess.PIPE)
-        except subprocess.CalledProcessError:
-            check_output = subprocess.check_output(f"amispammer -i {ip}", shell=True, stdin=subprocess.PIPE,
-                                                   stderr=subprocess.PIPE)
+
+        check_output = subprocess.check_output(f"amispammer -i {ip}", shell=True, stdin=subprocess.PIPE,
+                                               stderr=subprocess.PIPE)
 
         lines = check_output.decode().split('\n')
         ips["checked ips"].append(ip)
 
         if 'REASON' in check_output.decode():
             spam_ip = lines[2].split(":")[1]
+            # the change in iptables not permanent, so save it or ignore before rebooting the system .. it's up to you
             subprocess.Popen(f"sudo iptables -A INPUT -s {spam_ip} -j DROP", shell=True, stdin=subprocess.PIPE,
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            subprocess.Popen(f"sudo iptables -A INPUT -d {spam_ip} -j DROP", shell=True, stdin=subprocess.PIPE,
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
             print("----------> Blocked IP : ", spam_ip)
             ips["blacklisted"].append(spam_ip)
     ips["checking ips"] = []
